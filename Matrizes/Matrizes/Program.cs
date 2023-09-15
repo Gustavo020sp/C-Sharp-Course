6ab61e8fe95c88895b27872938c6f5541af8b995
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-09-15 11:22:34 -0300

    Exercício Matrizes

---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex. 1 - imprimir na tela diagonal principal e quantidade valores negativos da matriz
+
+            //Console.WriteLine("Digite o tamanho da matriz: ");
+            //int n = Convert.ToInt32((Console.ReadLine()));
+            //int[,] mat = new int[n, n];
+
+            //for (int linha = 0; linha < n; linha++)
+            //{
+
+            //    string[] linhavetor = Console.ReadLine().Split(' ');
+
+            //    for (int coluna = 0; coluna < n; coluna++)
+            //    {
+            //        mat[linha, coluna] = Convert.ToInt32(linhavetor[coluna]);
+            //    }
+
+            //}
+
+            //Console.WriteLine("Main Diagonal: ");
+
+            //for (int i =0; i < n; i++) {
+            //    Console.Write(mat[i, i] + " ");
+            //}
+
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            ////Console.WriteLine("Negative numbers: ");
+
+            //int count = 0;
+
+            //for (int linha = 0; linha < n; linha++)
+            //{
+
+            //    for (int coluna = 0; coluna < n; coluna++)
+            //    {
+            //        if (mat[linha, coluna] < 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+
+            //}
+
+            //Console.WriteLine("Negative numbers = " + (count));
+
+            //Console.ReadLine();
+
+
+            //--------------------------------------------------------------------//
+            //                        Exercicio fixação                           //
+
+            Console.Write("Digite a matriz (linha): ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a matriz (coluna): ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matriz = new int[l, c];
+
+            for (int linha = 0; linha < l; linha ++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+
+                for (int coluna = 0; coluna < c; coluna++)
+                {
+                    matriz[linha, coluna] = Convert.ToInt32(vetor[coluna]);
+                }
+            }
+
+            Console.Write("Selecione um número da matriz: ");
+            int escolhanumero = Convert.ToInt32(Console.ReadLine());
+
+            for (int linha = 0; linha < l; linha++)
+            {
+                for (int coluna = 0; coluna < c; coluna++)
+                {
+                    if (matriz[linha, coluna] == escolhanumero)
+                    {
+                        Console.WriteLine("Position: " + linha + ", " + coluna);
+
+                        if (coluna > 0)
+                        {
+                            Console.WriteLine("Left: " + matriz[linha, (coluna - 1)]);
+                        }
+                        if (coluna < l - 1)
+                        {
+                            Console.WriteLine("Right: " + matriz[linha, (coluna + 1)]);
+                        }
+                        if (linha > 0)
+                        {
+                            Console.WriteLine("Up: " + matriz[(linha - 1), coluna]);
+                        }
+                        if (linha < c - 1)
+                        {
+                            Console.WriteLine("Down: " + matriz[(linha + 1), coluna]);
+                        }
+
+                    }
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Matrizes/Matrizes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Matrizes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FIS")]
+[assembly: AssemblyProduct("Matrizes")]
+[assembly: AssemblyCopyright("Copyright © FIS 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6ebd9d9-e529-4978-a802-3ccfe6f43673")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
