fbdadad0fb7afb61fdbea7ef0462ae9d1f373ebf
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-09-15 11:16:05 -0300

    Exercícios construtores

---
+++ Construtores/Construtores/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+    public class Produto
+    {
+        private string nome;
+        public double preco { get; private set; }   
+        public int quantidade { get; private set; }
+
+        public string Nome
+        {
+            get { return nome; }
+            set {
+                if (value != null && value.Length < 1)
+                {
+                    nome = value;
+                }
+            }
+        }
+
+        public double Total()
+        {
+            return quantidade * preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            this.quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            this.quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return nome + ", $" + preco + ", " + quantidade + " unidades, total = $" + Total() ;
+        }
+
+    }
+}
---
+++ Construtores/Construtores/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre com os dados do produto: ");
+            string nome = Console.ReadLine();
+            double preco = Convert.ToInt32(Console.ReadLine());
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("");
+
+            Console.Write("Dados do produto: " + produto.ToString());
+
+            Console.WriteLine("");
+
+            Console.Write("Digite o número de produtos a serem adicionados: ");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+            produto.AdicionarProdutos(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + produto.ToString());
+
+            Console.Write("Digite o número de produtos a serem removidos: ");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+            produto.RemoverProdutos(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + produto.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
