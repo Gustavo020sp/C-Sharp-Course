d6205d73f866861be3c1d657eab59406a545dcbc
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-09-15 11:21:45 -0300

    Exercício Vetores

---
+++ Exercício_Vetores/Exercício_Vetores/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nRooms;
+            string name;
+            string email;
+            int room = 0;
+
+            Rent[] vect = new Rent[10];
+
+            Console.Write("How many rooms will be rented? ");
+            nRooms = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nRooms; i++)
+            {
+                Console.WriteLine("Rent #" + (i+1));
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Room: ");
+                room = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                vect[room] = new Rent();
+                vect[room].Name = name;
+                vect[room].Email = email;
+                vect[room].Room = room;    
+            }
+
+            Console.WriteLine("Busy rooms: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ":" + vect[i].Name + ", " + vect[i].Email);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Exercício_Vetores/Exercício_Vetores/Rent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Vetores
+{
+    internal class Rent
+    {
+        public string Name { get; set; }    
+        public string Email { get; set; }
+        public int Room { get; set; }
+    }
+}
