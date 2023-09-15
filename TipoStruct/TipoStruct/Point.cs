103897e86287f4c0e1b9ecf74f9d8085191ffb3c
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-09-15 11:34:07 -0300

    Exercício Structs

---
+++ TipoStruct/TipoStruct/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoStruct
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ")" +","+ "(" + Y + ")";
+        }
+    }
+}
---
+++ TipoStruct/TipoStruct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double? x = null;
+            double? y = 10;
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("O Valor de x é nullo, verifique");
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("erro");
+
+            Console.ReadLine();
+
+        }
+    }
+}
