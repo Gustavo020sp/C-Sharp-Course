51fc4970eec41137c4f5bcac363fa08e4fa2c959
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-09-15 11:18:25 -0300

    Exercícios Foreach

---
+++ Exemplo_For_Each/Exemplo_For_Each/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_For_Each
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] vect = new string[] { "Maria", "Bob", "Kaua" };
+
+            foreach(var obj in vect) { Console.WriteLine(obj); }
+
+            Console.ReadLine();
+        }
+    }
+}
