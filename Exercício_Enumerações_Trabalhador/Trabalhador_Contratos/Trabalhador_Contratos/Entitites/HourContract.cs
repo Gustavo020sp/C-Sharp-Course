b55917350be0f169cd9fe478ee72dd510f0274a2
Gomes, Gustavo <gustavo.a.gomes@fisglobal.com>
2023-10-02 10:50:04 -0300

    Exercício Worker

---
+++ Exercício_Enumerações_Trabalhador/Trabalhador_Contratos/Trabalhador_Contratos/Entitites/HourContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalhador_Contratos.Entitites
+{
+    public class HourContract
+    {
+        DateTime date;
+        double valuePerHour;
+        int hours;
+    }
+
+    //public double totalValue()
+    //{
+
+    //}
+}
---
+++ Exercício_Enumerações_Trabalhador/Trabalhador_Contratos/Trabalhador_Contratos/Entitites/Trabalhador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalhador_Contratos.Entitites.Enums;
+
+namespace Trabalhador_Contratos.Entitites
+{
+    public class Trabalhador
+    {
+        public string name { get; set; }
+        public WorkerLevel level { get; set; }
+        public double baseSalary { get; set; }
+
+        //public override string ToString()
+        //{
+        //    //return "Name:" + this.name + "Department: " + depName ToString(); 
+        //}
+    }
+}
