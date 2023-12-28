cf4b9f358ded85cc0d2492bbcc9c9b463ab6af3e
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-12-28 08:24:50 -0300

    Add files via upload

---
+++ Tratamento_De_Exceções_2.0/Tratamento_De_Exceções_2.0/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratamento_De_Exceções_2._0.Entities
+{
+    public class Account
+    {
+        public int number { get; set; }
+        public string holder { get; set; }
+        public double balance { get; set; }
+        public double withdrawlimit { get; set; }
+        //public double amount { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawlimit)
+        {
+            this.number = number;
+            this.holder = holder;
+            this.balance = balance;
+            this.withdrawlimit = withdrawlimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (withdrawlimit < amount)
+            {
+                throw new Exception("Withdraw error: The amount exceeds withdraw limit! ");
+            }
+            if(withdrawlimit > balance)
+            {
+                throw new Exception("Withdraw error: Not enough balance!! ");
+            }
+
+            balance = balance - amount;
+        }
+
+        public override string ToString()
+        {
+            return $"New balance: $ {balance}".ToString();
+        }
+    }
+}
---
+++ Tratamento_De_Exceções_2.0/Tratamento_De_Exceções_2.0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tratamento_De_Exceções_2._0.Entities;
+
+namespace Tratamento_De_Exceções_2._0
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double amount;
+
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdrawlimit = Convert.ToDouble(Console.ReadLine());
+
+                //instanciando nova conta com dados inseridos pelo usuário
+                Account account = new Account(number, holder, balance, withdrawlimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                amount = Convert.ToDouble((string)Console.ReadLine());
+                account.Withdraw(amount);
+                Console.WriteLine(account.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
