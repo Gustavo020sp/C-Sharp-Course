50245f0bfc953af531e1415289f11e7ec9b7de03
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-12-28 08:25:10 -0300

    Add files via upload

---
+++ Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalhando_Com_Arquivos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\e5684672\OneDrive - FIS\Desktop\Para subir no github\Trabalhando_Com_Arquivos\NewTextDocument.txt";
+            string targetPath = @"C:\Users\e5684672\OneDrive - FIS\Desktop\Para subir no github\Trabalhando_Com_Arquivos\NewTextDocument2.txt";
+
+            try
+            {
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);  
+            }
+        }
+    }
+}
