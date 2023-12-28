cec93d6c4756e144fdc16fe50f660f9e64ca6d7d
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2023-12-28 08:24:30 -0300

    Add files via upload

---
+++ Tratamento_De_Exceções/Tratamento_De_Exceções/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Tratamento_De_Exceções.Entities
+{
+    public class Reservation
+    {
+        public int roomNumber { get; set; }
+        public DateTime checkin { get; set; }
+        public DateTime checkout { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new Exception("verifique...");
+            }
+            
+            this.roomNumber = roomNumber;
+            this.checkin = checkin;
+            this.checkout = checkout;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = checkout.Subtract(checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new Exception("Erro na data. verifique");
+            }
+            this.checkin = checkin;
+            this.checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"Reservation: Room {roomNumber}, check-in: {checkin.ToString("dd/MM/yyyy")}, checkout: {checkout.ToString("dd/MM/yyyy")}, {Duration()} night(s).".ToString();
+        }
+
+    }
+}
---
+++ Tratamento_De_Exceções/Tratamento_De_Exceções/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tratamento_De_Exceções.Entities;
+
+namespace Tratamento_De_Exceções
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime checkin = DateTime.Now;
+            DateTime checkout = DateTime.Now;
+
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Check-In date (dd/MM/yyy): ");
+                checkin = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyy): ");
+                checkout = Convert.ToDateTime(Console.ReadLine());
+                Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+                Console.Write(reservation.ToString());
+
+                Console.WriteLine();
+
+                Console.Write("Enter data to update the reservation");
+                Console.Write("Check-In date (dd/MM/yyy): ");
+                checkin = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyy): ");
+                checkout = Convert.ToDateTime(Console.ReadLine());
+                reservation.UpdateDates(checkin, checkout);
+                Console.Write(reservation.ToString());
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please insert number room.");
+                Console.ReadLine();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Incorret data. ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
