1f955ba5dd9dcfb09cf008003b06f9fc3daa57b8
Gomes, Gustavo <Gustavo.A.Gomes@fisglobal.com>
2024-01-24 07:48:45 -0300

    começo da atividade

--- Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Program.cs
+++ Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Program.cs
@@ -7,16 +7,20 @@ using System.Threading.Tasks;
 
 namespace Trabalhando_Com_Arquivos
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            string sourcePath = @"C:\Users\e5684672\OneDrive - FIS\Desktop\Para subir no github\Trabalhando_Com_Arquivos\NewTextDocument.txt";
-            string targetPath = @"C:\Users\e5684672\OneDrive - FIS\Desktop\Para subir no github\Trabalhando_Com_Arquivos\NewTextDocument2.txt";
+            string sourcePath = @"C:\Github\C-Sharp-Course\Trabalhando_Com_Arquivos\NewTextDocument.txt";
+            string outputPath = @"C:\Github\C-Sharp-Course\Trabalhando_Com_Arquivos\NewTextDocument2.txt";
+
+            FileInfo fileInfo = new FileInfo(sourcePath);
+
+            string[] files = File.ReadAllLines(sourcePath);
 
             try
             {
-
+                //list.Add(sourcePath.ToLower);
             }
             catch (IOException e)
             {
