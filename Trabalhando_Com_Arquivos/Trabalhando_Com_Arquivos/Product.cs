c755cf2b4df71b940ee33dc9383be21d80cb39e7
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-02-24 11:22:41 -0300

    Code update

---
+++ Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalhando_Com_Arquivos
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }   
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return price * quantity;
+        }
+        
+    }
+}
--- Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Program.cs
+++ Trabalhando_Com_Arquivos/Trabalhando_Com_Arquivos/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +14,34 @@ namespace Trabalhando_Com_Arquivos
         public static void Main(string[] args)
         {
             string sourcePath = @"C:\Github\C-Sharp-Course\Trabalhando_Com_Arquivos\NewTextDocument.txt";
-            string outputPath = @"C:\Github\C-Sharp-Course\Trabalhando_Com_Arquivos\NewTextDocument2.txt";
-
-            FileInfo fileInfo = new FileInfo(sourcePath);
-
-            string[] files = File.ReadAllLines(sourcePath);
+            string targetPath = @"C:\Github\C-Sharp-Course\Trabalhando_Com_Arquivos\Summary.csv";
 
             try
             {
-                //list.Add(sourcePath.ToLower);
+                string[] lines = File.ReadAllLines(sourcePath);
+                
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1]);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.name + "," + prod.Total().ToString());
+                    }
+                }
+
             }
             catch (IOException e)
             {
                 Console.WriteLine(e.Message);  
             }
+           
+            Console.ReadLine();
         }
     }
 }
