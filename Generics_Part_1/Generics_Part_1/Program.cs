25cfacfc02a47a5cbcfe89dbd1e2d236cf173491
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-02-29 23:54:41 -0300

    Generics_Examples

---
+++ Generics_Part_1/Generics_Part_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Generics_Part_1.Services;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Generics_Part_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("How many values? ");
+            int values = int.Parse(Console.ReadLine());
+
+            PrintService<string> printService = new PrintService<string>(values);
+
+            printService.addValue(values);
+            printService.print();
+        }
+    }
+}
---
+++ Generics_Part_1/Generics_Part_1/Services/PrintService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Part_1.Services
+{
+    public class PrintService<T>
+    {
+        public string convertedlist;
+        public int values;
+        List<string> listvalues = new List<string>();
+
+        public PrintService(int values)
+        { 
+            this.values = values;
+        }
+
+        public void addValue(int values)
+        {
+            
+
+            for (int i = 0; i < values; i++)
+            {
+                listvalues.Add(Console.ReadLine());
+            }
+
+            convertedlist = string.Join(", ", listvalues.ToArray());
+        }       
+        public void print()
+        {
+            Console.WriteLine("[" + convertedlist + "]\n"
+            + "First: " + listvalues.First());
+        }
+    }
+}
