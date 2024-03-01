1c640d8591964e4dd52524164906770fec526168
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-03-01 16:41:29 -0300

    Criação do projeto

---
+++ Projeto_Xadrez_ConsoleApp/Projeto_Xadrez_ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_Xadrez_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
