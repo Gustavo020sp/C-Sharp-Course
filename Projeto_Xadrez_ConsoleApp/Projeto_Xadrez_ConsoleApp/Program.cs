b83b9676518fcb64cc6e687b855b32aff6cf7a33
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-03-01 22:40:02 -0300

    Implementação do método imprimeTabuleiro()

--- Projeto_Xadrez_ConsoleApp/Projeto_Xadrez_ConsoleApp/Program.cs
+++ Projeto_Xadrez_ConsoleApp/Projeto_Xadrez_ConsoleApp/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Projeto_Xadrez_ConsoleApp.tabuleiro;
 
-namespace Projeto_Xadrez_ConsoleApp.tabuleiro
+namespace Projeto_Xadrez_ConsoleApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
         }
     }
 }
---
+++ Projeto_Xadrez_ConsoleApp/Projeto_Xadrez_ConsoleApp/Tela.cs
@@ -0,0 +1,27 @@
+﻿using Projeto_Xadrez_ConsoleApp.tabuleiro;
+
+namespace Projeto_Xadrez_ConsoleApp
+{
+    public class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for(int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine("-");
+            }
+        }
+        
+    }
+}
