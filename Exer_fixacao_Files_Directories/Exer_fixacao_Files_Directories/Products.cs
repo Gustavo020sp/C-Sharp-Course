5dd0ec8c3780d91587c19552cb81ddaaf3abbb52
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-03-16 11:05:11 -0300

    Update -> FIle_Derectories Oficial

---
+++ Exer_fixacao_Files_Directories/Exer_fixacao_Files_Directories/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer_fixacao_Files_Directories
+{
+    public class Products
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+        
+        public Products(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public double Total()
+        {
+            return price * quantity;   
+        }
+    }
+}
---
+++ Exer_fixacao_Files_Directories/Exer_fixacao_Files_Directories/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+
+namespace Exer_fixacao_Files_Directories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> products = new List<string>();
+            string sourcePath = @"C:\Gustavo\Products\productList.txt";
+            string outputPath = @"C:\Gustavo\Products\Out\summary.csv";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                //a partir daqui ja tenho o vetor preenchido
+                using (StreamWriter sw = new StreamWriter(outputPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Products prod = new Products(name, price, quantity);
+                        sw.WriteLine(prod.name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    } 
+                }                       
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERRO: \n" + e.Message);
+            }
+        }
+    }
+}
