17d2a18fa8dfaeb65c2300819b589ddc40af5fd5
Gustavo Gomes <gustavo020sp@gmail.com>
2024-03-22 12:28:30 -0300

    started implementing the code

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -4,8 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //pending code below
+            Dictionary<string, int> cookies = new Dictionary<string, int>();
+            //cookies["user"] = "maria";
+            //cookies["password"] = "1234";
+            //cookies["email"] = "maria@";
+            //cookies["email"] = "maria2@";
+            //Console.WriteLine(cookies["email"]);
+            //D:\Projects\C-Sharp-Course\Dictionary\Candidates\candidates.txt
+
+            Console.WriteLine("Enter file path: ");
+            string filepath = Console.ReadLine();
+
+            using(StreamReader sr = new StreamReader(filepath)) 
+            {
+                File.ReadAllLines(File.ReadAllText(filepath));
+                while(!sr.EndOfStream)
+                {
+                    
+                }
+            }
+
+            Console.ReadLine();
+            
         }
     }
 }
