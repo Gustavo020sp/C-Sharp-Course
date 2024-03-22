976f960218dc488fc4d624c07cfd1f02f8bdee51
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-03-21 21:49:58 -0300

    Add files via upload

---
+++ Set2_Resolution/Set2_Resolution/Entities/StudentRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set2_Resolution.Entities
+{
+    public class StudentRecord
+    {
+        public int Id { get; set; }
+
+        //public StudentRecord(int id)
+        //{
+        //    Id = id;
+        //}
+
+        //aqui vai verificar o hashcode do elemento, se forem valores iguais gera o mesmo hashcode
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        //finalmente, pra ter certeza usa o equals
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is StudentRecord))
+            {
+                return false;
+            }
+            StudentRecord other = obj as StudentRecord;
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ Set2_Resolution/Set2_Resolution/Program.cs
@@ -0,0 +1,55 @@
+﻿using Set2_Resolution.Entities;
+using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace Set2_Resolution
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n_students;
+            int idStudent;
+
+            //List<int> id = new List<int>();
+            //para comparar se tem igual(ATÉ pra tirar duplicados), ao invés de LISTA, usar HASHSET !!!! // e Implementar GetHashCode,e Equals 
+            //os dois override, para dar certo, conforme as regras na classe StudentRecord!!!! //
+
+            HashSet<StudentRecord> id = new HashSet<StudentRecord>();
+
+            Console.Write("How many students for course A? ");
+            n_students = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n_students; i++)
+            {
+                idStudent = int.Parse(Console.ReadLine());
+                id.Add(new StudentRecord { Id = idStudent }); ; 
+            }
+
+            Console.Write("How many students for course B? ");
+            n_students = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n_students; i++)
+            {
+                idStudent = int.Parse(Console.ReadLine());
+                id.Add(new StudentRecord { Id = idStudent }); ;
+            }
+
+            Console.Write("How many students for course C? ");
+            n_students = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n_students; i++)
+            {
+                idStudent = int.Parse(Console.ReadLine());
+                id.Add(new StudentRecord { Id = idStudent }); ;
+            }
+
+            Console.WriteLine("Total students: " + id.Count);
+
+            Console.ReadLine();
+        }        
+    }
+}
+
