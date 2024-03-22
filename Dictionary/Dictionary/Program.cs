2fbfa1a61025d3083e5b4f221225e024d188f911
Gustavo Gomes <gustavo020sp@gmail.com>
2024-03-21 23:17:28 -0300

    Initial Commit

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //pending code below
+        }
+    }
+}
