cb5c21445cffadbf78d685381a1750fc3f6e1d1f
Gustavo Gomes <111130726+Gustavo020sp@users.noreply.github.com>
2024-03-21 21:51:25 -0300

    Trabalhando com HashSet / GetHashCode / Equals

---
+++ OnlyTest_GetHashCode/OnlyTest_GetHashCode/Entities/NameRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyTest_GetHashCode.Entities
+{
+    public class NameRecord
+    {
+        public string name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is NameRecord))
+            {
+                return false;
+            }
+            NameRecord other = obj as NameRecord;
+            return name.Equals(other.name);
+        }
+    }
+}
---
+++ OnlyTest_GetHashCode/OnlyTest_GetHashCode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OnlyTest_GetHashCode.Entities;
+
+
+namespace OnlyTest_GetHashCode
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            HashSet<NameRecord> listNames = new HashSet<NameRecord>();
+
+            Console.Write("How many names to the list? ");
+            int numberNames = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberNames; i++) 
+            {
+                name = Console.ReadLine();
+                listNames.Add(new NameRecord { name = name });
+            }
+
+            Console.WriteLine("Total Names: " +  listNames.Count);
+            Console.ReadLine();
+        }
+    }
+}
