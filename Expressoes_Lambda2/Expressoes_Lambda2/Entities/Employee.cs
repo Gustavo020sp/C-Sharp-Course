7d7ecd2b3976e925d22b3f19637d4e82297ece9c
Gustavo Gomes <gustavo020sp@gmail.com>
2024-03-23 13:01:20 -0300

    implemented streamreader and added to new list

---
+++ Expressoes_Lambda2/Expressoes_Lambda2/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressoes_Lambda2.Entities
+{
+    public class Employee
+    {
+        public string Name {  get; set; }
+        public string Email {  get; set; }
+        public double Salary {  get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
--- Expressoes_Lambda2/Expressoes_Lambda2/Program.cs
+++ Expressoes_Lambda2/Expressoes_Lambda2/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Expressoes_Lambda2
+﻿using Expressoes_Lambda2.Entities;
+using System.Globalization;
+
+namespace Expressoes_Lambda2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = @"D:\Projects\C-Sharp-Course\Expressoes_Lambda2\employees.csv";
+            List<Employee> list = new List<Employee>();
+            string[] employeeList;
+
+
+            using(StreamReader sr = new StreamReader(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    employeeList = sr.ReadLine().Split(",");
+                    string name = employeeList[0];
+                    string email = employeeList[1];
+                    double salary = Double.Parse(employeeList[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
         }
     }
 }
