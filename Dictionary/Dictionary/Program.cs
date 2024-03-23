3610f784348c037075b1753a2132ec7c79d34c94
Gustavo Gomes <gustavo020sp@gmail.com>
2024-03-23 00:05:26 -0300

    implementação do uso do Dictionary

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Dictionary<string, int> cookies = new Dictionary<string, int>();
+            // string[] Candidates;
+            //cookies["Candidate"] = ""
             //cookies["user"] = "maria";
             //cookies["password"] = "1234";
             //cookies["email"] = "maria@";
@@ -17,10 +19,25 @@
 
             using(StreamReader sr = new StreamReader(filepath)) 
             {
-                File.ReadAllLines(File.ReadAllText(filepath));
+                File.ReadAllLines(filepath);
                 while(!sr.EndOfStream)
                 {
-                    
+                    string[] Candidates = sr.ReadLine().Split(",");
+                    string name = Candidates[0];
+                    int votes  = int.Parse(Candidates[1]);
+
+                    if(cookies.ContainsKey(name))
+                    {
+                        cookies[name] += votes;
+                    }
+                    else
+                    {
+                        cookies[name] = votes;
+                    }                    
+                }
+                foreach (var item in cookies)
+                {
+                    Console.WriteLine(item.Key + "=" + item.Value);
                 }
             }
 
