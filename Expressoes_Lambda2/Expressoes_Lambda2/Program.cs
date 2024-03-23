93fbe89a38c0d7f065326d2437c600b86fe4b0bf
Gustavo Gomes <gustavo020sp@gmail.com>
2024-03-23 13:31:21 -0300

    code update

--- Expressoes_Lambda2/Expressoes_Lambda2/Program.cs
+++ Expressoes_Lambda2/Expressoes_Lambda2/Program.cs
@@ -8,7 +8,7 @@ namespace Expressoes_Lambda2
         static void Main(string[] args)
         {
             string path = @"D:\Projects\C-Sharp-Course\Expressoes_Lambda2\employees.csv";
-            List<Employee> list = new List<Employee>();
+            List<Employee> listEmployee = new List<Employee>();
             string[] employeeList;
 
 
@@ -20,9 +20,33 @@ namespace Expressoes_Lambda2
                     string name = employeeList[0];
                     string email = employeeList[1];
                     double salary = Double.Parse(employeeList[2], CultureInfo.InvariantCulture);
-                    list.Add(new Employee(name, email, salary));
+                    listEmployee.Add(new Employee(name, email, salary));
                 }
             }
+
+            //this will select in the list the email of people whose salary is greater than 2000.00
+            Console.Write("Enter salary: ");
+            double salaryInput = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Email of people whose salary is greater than 2000.00: ");
+
+            //here will implement the query using lambda and linq
+            var emails = listEmployee.Where(e => e.Salary > salaryInput);
+
+            foreach(Employee employee in emails )
+            {
+                Console.WriteLine(employee.Email);
+            }
+
+
+            //falta arrumar código abaixo para imprimir certo
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': ");
+
+            var sum = listEmployee.Select(p => p.Salary).DefaultIfEmpty(0.0).Sum();
+
+            Console.WriteLine(sum.ToString());
+
+            Console.ReadLine();
+
         }
     }
 }
