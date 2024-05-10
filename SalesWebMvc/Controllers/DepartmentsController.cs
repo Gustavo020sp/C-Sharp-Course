4ee9f9e67c080b7ea2b20119aeab6b68fd03a94c
Gustavo Gomes <gustavo020sp@gmail.com>
2024-05-09 23:42:04 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        List<Department> list = new List<Department>();
+        public IActionResult Index()
+        {           
+            return View(list);
+        }
+    }
+}
