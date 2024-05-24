abb2d96ca543c77b40abddc4d5a5137b8eb5fe4f
Gustavo Gomes <gustavo020sp@gmail.com>
2024-05-24 10:41:43 -0300

    No Service Update

---
+++ SalesWebMvc_NoService/SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus
+    {
+        Pending,
+        Billed,
+        Canceled
+    }
+}
---
+++ SalesWebMvc_NoService/SalesWebMvc/Models/SellerFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc.Models
+{
+	public class SellerFormViewModel
+	{
+		public Seller? seller {  get; set; }
+		public ICollection<Department>? Departments { get; set; }
+	}
+}
